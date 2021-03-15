12cc8b6a472db622d381bf72fa462db28f72b0e8
Rishabh Bisht <ribisht@oradev.oraclecorp.com>
2021-03-15 20:37:28 +0530

    next build configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Section1 from '../components/organism/MainPage/Section1';
 import Section2 from '../components/organism/MainPage/Section2';
-import Section3 from '../components/organism/MainPage/Section3';
-import Section4 from '../components/organism/MainPage/Section4';
 import { Divider } from '@chakra-ui/react';
 
 export default function Home() {
